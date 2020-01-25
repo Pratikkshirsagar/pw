149f600d7d41792f612ef844d0dbb7a4341d02d4
Pratik Kshirsagar <pratik@Pratiks-MacBook-Pro.local>
2020-01-25 21:29:47 +0530

    Inital working of password generater done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,58 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeLetters = false;
+  includeNumber = false;
+  includesymbols = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    const parseValue = parseInt(value);
+
+    if (!isNaN(parseValue)) {
+      this.length = parseValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumber = !this.includeNumber;
+  }
+
+  onChangeUseSymbols() {
+    this.includesymbols = !this.includesymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+
+    if (this.includeNumber) {
+      validChars += numbers;
+    }
+
+    if (this.includesymbols) {
+      validChars += numbers;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
 }
